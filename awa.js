04bfc9d078eba29e206fffeabb934c1611ea0248
lo- <lo-@naokiMacBook-Air.local>
2022-03-19 03:28:52 +0900

    あわのこーどがきれになたよ

--- awa.js
+++ awa.js
@@ -1,53 +1,63 @@
-(function(d,b,w){
-    var q = d.createElement('div');
-    q.innerHTML = '<style>html,body{overflow-x:hidden;}.bubble{position:absolute;background:rgba(255,255,255,0.1);border:thin solid rgba(255,255,225,0.3);border-radius:8px;box-shadow: 0 0 1px 1px rgba(255,255,225,0.2);}'+
-    '.bubble::after{content:"";display:block;height:3px;width:3px;border-radius:2px;background:rgba(255,255,255,0.2);}</style>';
+(function(d, b, w) {
+    const q = d.createElement('div');
+    q.innerHTML = `<style>
+    html,body{overflow-x:hidden;}
+    .bubble{
+      position:absolute;
+      background:rgba(255,255,255,0.1);
+      border:thin solid rgba(255,255,225,0.3);
+      border-radius:8px;
+      box-shadow: 0 0 1px 1px rgba(255,255,225,0.2);
+    }
+    .bubble::after{
+      content:"";
+      display:block;
+      height:3px;
+      width:3px;b
+      order-radius:2px;
+      background:rgba(255,255,255,0.2);
+    }</style>`;
     q.id = 'bubbleparticle';
     b.appendChild(q);
-    q = d.getElementById('bubbleparticle');
-    var h = w.innerHeight;
-    var u = d.documentElement.scrollTop || b.scrollTop;
-    var z = 9999;
-    var t = new Array();
-    var l = new Array();
-    var y = new Array();
-    var s = new Array();
-    var g = new Array();
-    var c = new Array();
-    d.addEventListener('scroll',function(){u = d.documentElement.scrollTop || b.scrollTop;},false);
-    for(var i=0;i<50;i++){//【1】泡の量を指定
-      var m = d.createElement('div');
-      m.id = 'awa'+i;
-      t[i] = Math.random()*h+u;
-      l[i] = Math.random()*w.innerWidth;
-      var p = Math.random()*8+6;
-      m.setAttribute('style','z-index:'+(z+i)+';top:'+t[i]+'px;width:'+p+'px;height:'+p+'px;left:'+l[i]+'px;');
-      m.setAttribute('class','bubble');
-      q.appendChild(m);
-      y[i] = Math.random()*25+0.1;
-      s[i] = Math.random()*5+0.5;//【2】泡の上昇速度
-      g[i] = d.getElementById('awa'+i);
-      c[i] = 0;
+
+    const h = w.innerHeight;
+    let u = d.documentElement.scrollTop || b.scrollTop;
+    const z = 9999;
+    const top = new Array();
+    const left = new Array();
+    const velocityY = new Array();
+    const g = new Array();
+
+    d.addEventListener('scroll', () => { u = d.documentElement.scrollTop || b.scrollTop; }, false);
+    for (let i = 0; i < 50; i++) { //【1】泡の量を指定
+        const m = d.createElement('div');
+        q.appendChild(m);
+        const p = Math.random() * 8 + 6;
+        m.setAttribute('style', 'z-index:' + (z + i) + ';top:' + top[i] + 'px;width:' + p + 'px;height:' + p + 'px;left:' + left[i] + 'px;');
+        m.setAttribute('class', 'bubble');
+
+        top[i] = Math.random() * h + u;
+        left[i] = Math.random() * w.innerWidth;
+        velocityY[i] = Math.random() * 5 + 1; //【2】泡の上昇速度
+        g[i] = m;
     }
-    setInterval(function(){
-      for(var i=0;i<50;i++){//【1】泡の量を指定
-        if(u<t[i]){
-          if(y[i]>=c[i]){
-            l[i] = l[i]+0.5+Math.random()*0.5;
-          }else{
-            l[i] = l[i]-0.5-Math.random()*0.5;
-          }
-          if((y[i]*2)<=c[i]){
-            c[i] = 0;
-          }
-        }else{
-            t[i] = u+h-10;
-            l[i] = Math.random()*w.innerWidth;
-          }
-          t[i] = t[i]-s[i];
-          g[i].style.top = t[i]+'px';
-          g[i].style.left = l[i]+'px';
-          c[i]++;
+    setInterval(function() {
+        for (let i = 0; i < g.length; i++) { //【1】泡の量を指定
+            left[i] += (Math.random() - 0.5) * 2;
+            top[i] -= velocityY[i];
+
+            if (top[i] < u) {
+                top[i] = u + h - 10;
+            }
+            if (left[i] < 0) {
+                left[i] = Math.random() + w.innerWidth;
+            }
+            if (left[i] > w.innerWidth) {
+                left[i] = Math.random()
+            }
+
+            g[i].style.top = top[i] + 'px';
+            g[i].style.left = left[i] + 'px';
         }
-      },45);//【3】繰り返しまでの数値
-    })(document,document.body,window);
+    }, 45); //【3】繰り返しまでの数値
+})(document, document.body, window);
